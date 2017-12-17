d04baaf214e4d453efe0e2d6a3cb62f3090cb305
tcrobert <tcroberts@live.ca>
2017-12-17 15:57:44 -0800

    Bugfix & update licenses & setup.py templates

--- cookiecutter.sh
+++ cookiecutter.sh
@@ -147,7 +147,7 @@ then
       git remote add origin $GITURL
       git push -u origin master
     else
-      echo "Uh-oh. Something went wrong with the URL you entered. Repository not pushed to GitHub."
+      echo "Uh-oh. Something went wrong with the URL you entered. Remote repository not initialized."
     fi
   fi
   if [ $GITNAME = "NONE" ] # sets $GITNAME back to null if NONE
@@ -159,79 +159,108 @@ fi
 # -------------- Set up License based on previously set type ------------- #
 if [ $LICTYPE = "BSD" ]
 then
-  echo "Copyright (c) $YEAR, $USER
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-      * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above copyright
-        notice, this list of conditions and the following disclaimer in the
-        documentation and/or other materials provided with the distribution.
-      * Neither the name of the <organization> nor the
-        names of its contributors may be used to endorse or promote products
-        derived from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." >> LICENSE
+  cat $HOMEPATH/assets/licenses/bsd.txt >> LICENSE
 elif [ $LICTYPE = "MIT" ]
 then
-  echo "Copyright (c) $YEAR $USER
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE." >> LICENSE
+  cat $HOMEPATH/assets/licenses/mit.txt >> LICENSE
 elif [ $LICTYPE = "AGPL3" ]
 then
-  cat $HOMEPATH/licenses/agpl3.txt >> LICENSE
+  cat $HOMEPATH/assets/licenses/agpl3.txt >> LICENSE
 elif [ $LICTYPE = "GPL" ]
 then
-  cat $HOMEPATH/licenses/gpl.txt >> LICENSE
+  cat $HOMEPATH/assets/licenses/gpl.txt >> LICENSE
 elif [ $LICTYPE = "LGPL" ]
 then
-  cat $HOMEPATH/licenses/lgpl.txt >> LICENSE
+  cat $HOMEPATH/assets/licenses/lgpl.txt >> LICENSE
 elif [ $LICTYPE == "MOZILLA" ]
 then
-  cat $HOMEPATH/licenses/mozilla.txt >> LICENSE
+  cat $HOMEPATH/assets/licenses/mozilla.txt >> LICENSE
 elif [ $LICTYPE == "APACHE" ]
 then
-  cat $HOMEPATH/licenses/unlicense.txt >> LICENSE
+  cat $HOMEPATH/assets/licenses/apache.txt >> LICENSE
 elif [ $LICTYPE == "UNLICENSE" ]
 then
-  cat $HOMEPATH/licenses/unlicense.txt >> LICENSE
+  cat $HOMEPATH/assets/licenses/unlicense.txt >> LICENSE
 else
-  # echo "$LICTYPE is not supported, would you like to try again?"
-  # read TRYAGAIN
-  # while [ $TRYAGAIN = "Y"] # Need to set up a check for $LICTYPE not in a list of values.
-  # do
-  #   read LICTYPE
-  #   if [ ! $LICTYPE ]
-  # done
-  echo "License not initialized -- License Type: '$LICTYPE' is not currently supported"
-fi
+#   echo "$LICTYPE is not supported, would you like to try again?"
+#   read TRYAGAIN
+#   while [ $TRYAGAIN = "Y"] # Need to set up a check for $LICTYPE not in a list of values.
+#   do
+#   contains() {
+#     [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
+#   }
+#      read LICTYPE
+#      if [ contains "BSD MIT AGPL3 GPL LGPL MOZILLA APACHE UNLICENSE" $LICTYPE ]
+#      then
+#        if [ $LICTYPE = "BSD" ]
+#        then
+#          echo "Copyright (c) $YEAR, $USER
+#          All rights reserved.
+#
+#          Redistribution and use in source and binary forms, with or without
+#          modification, are permitted provided that the following conditions are met:
+#              * Redistributions of source code must retain the above copyright
+#                notice, this list of conditions and the following disclaimer.
+#              * Redistributions in binary form must reproduce the above copyright
+#                notice, this list of conditions and the following disclaimer in the
+#                documentation and/or other materials provided with the distribution.
+#              * Neither the name of the <organization> nor the
+#                names of its contributors may be used to endorse or promote products
+#                derived from this software without specific prior written permission.
+#
+#          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+#          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#          DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+#          DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#          ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." >> LICENSE
+#        elif [ $LICTYPE = "MIT" ]
+#        then
+#          echo "Copyright (c) $YEAR $USER
+#
+#          Permission is hereby granted, free of charge, to any person obtaining a copy
+#          of this software and associated documentation files (the "Software"), to deal
+#          in the Software without restriction, including without limitation the rights
+#          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#          copies of the Software, and to permit persons to whom the Software is
+#          furnished to do so, subject to the following conditions:
+#
+#          The above copyright notice and this permission notice shall be included in all
+#          copies or substantial portions of the Software.
+#
+#          THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#          SOFTWARE." >> LICENSE
+#        elif [ $LICTYPE = "AGPL3" ]
+#        then
+#          cat $HOMEPATH/licenses/agpl3.txt >> LICENSE
+#        elif [ $LICTYPE = "GPL" ]
+#        then
+#          cat $HOMEPATH/licenses/gpl.txt >> LICENSE
+#        elif [ $LICTYPE = "LGPL" ]
+#        then
+#          cat $HOMEPATH/licenses/lgpl.txt >> LICENSE
+#        elif [ $LICTYPE == "MOZILLA" ]
+#        then
+#          cat $HOMEPATH/licenses/mozilla.txt >> LICENSE
+#        elif [ $LICTYPE == "APACHE" ]
+#        then
+#          cat $HOMEPATH/licenses/apache.txt >> LICENSE
+#        elif [ $LICTYPE == "UNLICENSE" ]
+#        then
+#          cat $HOMEPATH/licenses/unlicense.txt >> LICENSE
+#
+#   done
+#   echo "License not initialized -- License Type: '$LICTYPE' is not currently supported"
+# fi
 
 # --------------------- Set up Type-based Files/Folders ------------------- #
 if [ $TYPE = "DA" ]
