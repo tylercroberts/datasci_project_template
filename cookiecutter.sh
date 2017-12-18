8def2405f14023edadedb86053abca5b39436721
tcrobert <tcroberts@live.ca>
2017-12-17 16:01:16 -0800

    Fix error catch for lictype

--- cookiecutter.sh
+++ cookiecutter.sh
@@ -182,85 +182,44 @@ elif [ $LICTYPE == "UNLICENSE" ]
 then
   cat $HOMEPATH/assets/licenses/unlicense.txt >> LICENSE
 else
-#   echo "$LICTYPE is not supported, would you like to try again?"
-#   read TRYAGAIN
-#   while [ $TRYAGAIN = "Y"] # Need to set up a check for $LICTYPE not in a list of values.
-#   do
-#   contains() {
-#     [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
-#   }
-#      read LICTYPE
-#      if [ contains "BSD MIT AGPL3 GPL LGPL MOZILLA APACHE UNLICENSE" $LICTYPE ]
-#      then
-#        if [ $LICTYPE = "BSD" ]
-#        then
-#          echo "Copyright (c) $YEAR, $USER
-#          All rights reserved.
-#
-#          Redistribution and use in source and binary forms, with or without
-#          modification, are permitted provided that the following conditions are met:
-#              * Redistributions of source code must retain the above copyright
-#                notice, this list of conditions and the following disclaimer.
-#              * Redistributions in binary form must reproduce the above copyright
-#                notice, this list of conditions and the following disclaimer in the
-#                documentation and/or other materials provided with the distribution.
-#              * Neither the name of the <organization> nor the
-#                names of its contributors may be used to endorse or promote products
-#                derived from this software without specific prior written permission.
-#
-#          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
-#          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#          DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-#          DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#          ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." >> LICENSE
-#        elif [ $LICTYPE = "MIT" ]
-#        then
-#          echo "Copyright (c) $YEAR $USER
-#
-#          Permission is hereby granted, free of charge, to any person obtaining a copy
-#          of this software and associated documentation files (the "Software"), to deal
-#          in the Software without restriction, including without limitation the rights
-#          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#          copies of the Software, and to permit persons to whom the Software is
-#          furnished to do so, subject to the following conditions:
-#
-#          The above copyright notice and this permission notice shall be included in all
-#          copies or substantial portions of the Software.
-#
-#          THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#          SOFTWARE." >> LICENSE
-#        elif [ $LICTYPE = "AGPL3" ]
-#        then
-#          cat $HOMEPATH/licenses/agpl3.txt >> LICENSE
-#        elif [ $LICTYPE = "GPL" ]
-#        then
-#          cat $HOMEPATH/licenses/gpl.txt >> LICENSE
-#        elif [ $LICTYPE = "LGPL" ]
-#        then
-#          cat $HOMEPATH/licenses/lgpl.txt >> LICENSE
-#        elif [ $LICTYPE == "MOZILLA" ]
-#        then
-#          cat $HOMEPATH/licenses/mozilla.txt >> LICENSE
-#        elif [ $LICTYPE == "APACHE" ]
-#        then
-#          cat $HOMEPATH/licenses/apache.txt >> LICENSE
-#        elif [ $LICTYPE == "UNLICENSE" ]
-#        then
-#          cat $HOMEPATH/licenses/unlicense.txt >> LICENSE
-#
-#   done
-#   echo "License not initialized -- License Type: '$LICTYPE' is not currently supported"
-# fi
+  echo "$LICTYPE is not supported, would you like to try again?"
+  read TRYAGAIN
+  while [ $TRYAGAIN = "Y"] # Need to set up a check for $LICTYPE not in a list of values.
+  do
+  contains() {
+    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
+  }
+     read LICTYPE
+     if [ contains "BSD MIT AGPL3 GPL LGPL MOZILLA APACHE UNLICENSE" $LICTYPE ]
+     then
+       if [ $LICTYPE = "BSD" ]
+       then
+        cat $HOMEPATH/licenses/bsd.txt >> LICENSE
+       elif [ $LICTYPE = "MIT" ]
+       then
+        cat $HOMEPATH/licenses/mit.txt >> LICENSE
+       elif [ $LICTYPE = "AGPL3" ]
+       then
+         cat $HOMEPATH/licenses/agpl3.txt >> LICENSE
+       elif [ $LICTYPE = "GPL" ]
+       then
+         cat $HOMEPATH/licenses/gpl.txt >> LICENSE
+       elif [ $LICTYPE = "LGPL" ]
+       then
+         cat $HOMEPATH/licenses/lgpl.txt >> LICENSE
+       elif [ $LICTYPE == "MOZILLA" ]
+       then
+         cat $HOMEPATH/licenses/mozilla.txt >> LICENSE
+       elif [ $LICTYPE == "APACHE" ]
+       then
+         cat $HOMEPATH/licenses/apache.txt >> LICENSE
+       elif [ $LICTYPE == "UNLICENSE" ]
+       then
+         cat $HOMEPATH/licenses/unlicense.txt >> LICENSE # Think I still need a catch after this, just in case.
+      fi
+  done
+  echo "License not initialized -- License Type: '$LICTYPE' is not currently supported"
+fi
 
 # --------------------- Set up Type-based Files/Folders ------------------- #
 if [ $TYPE = "DA" ]
@@ -294,31 +253,7 @@ then
   # Create template for setup.py file
   if [ $GITNAME -n ]
   then
-    echo "import os
-from setuptools import setup, find_packages
-
-
-    def read(fname):
-        try:
-            return open(os.path.join(os.path.dirname(__file__), fname)).read()
-        except:
-            return 'Please see: https://github.com/$GITNAME/$PROJ'
-
-    setup(
-        name='$PROJ',
-        version='0.0.1',
-        author='$USER',
-        author_email='',
-        description='',
-        long_description=read('README.md'),
-        license='',
-        keywords='',
-        # download_url='',
-        packages=find_packages(),
-        install_requires=['scipy','numpy'],
-        classifiers=[],
-        include_package_data=True
-    )" >> setup.py
+    cat setup_template_t>> setup.py
 
     echo "Be sure to update the setup file with your dependencies"
   else
